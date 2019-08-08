8c00d888f9034d9edbbddfd4036c09e90d443443
Pyry Rouvila <pyry.rouvila@gmail.com>
2019-08-08 19:01:01 +0300

    Updates

---
+++ precache-manifest.1eb79bd396288710f6c85de7fc1d141b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab52460d308b290bc6ff97bd3d4106ce",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "53f95edf75b18508a368",
+    "url": "/dashboard/static/css/main.a96841a5.chunk.css"
+  },
+  {
+    "revision": "f71bcee28de78768c0e9",
+    "url": "/dashboard/static/js/2.5ea0d3ad.chunk.js"
+  },
+  {
+    "revision": "53f95edf75b18508a368",
+    "url": "/dashboard/static/js/main.c4fd9514.chunk.js"
+  },
+  {
+    "revision": "dcfbc05562333eb34d98",
+    "url": "/dashboard/static/js/runtime~main.26f3d777.js"
+  }
+]);
